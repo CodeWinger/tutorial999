370af2c40bc185f2a58862937723e8ae67349293
CodeWinger <mathieu.boucher5@gmail.com>
2019-10-11 14:56:06 -0400

    SonarCloud Integration

---
+++ src/main/java/tutorial999/tutorial999/App.java
@@ -0,0 +1,12 @@
+package tutorial999.tutorial999;
+
+public class App
+{
+    private int x;
+
+    public static void main( String[] args )
+    {
+    	String s = "Hello World";
+    	System.out.println(s.length());
+    }
+}
---
+++ src/test/java/tutorial999/tutorial999/AppTest.java
@@ -0,0 +1,11 @@
+package tutorial999.tutorial999;
+
+import org.junit.Test;
+
+public class AppTest
+{
+	@Test
+	public void mainTest() {
+	    App.main(null);
+	}
+}
